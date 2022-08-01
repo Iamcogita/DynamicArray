6e53ee92f7f42a3eb1c45539923a23a85fdb389b
David Mendes <david.mendes@mindswap.academy>
2022-08-01 16:54:21 +0100

    continuation of the exercise

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,34 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        MyArrays myArrays = new MyArrays();
+        MyArrays newArr = new MyArrays();
 
+        newArr.addArr("Hello");
+        newArr.addArr("World");
+        newArr.addArr("this");
+        newArr.addArr("is");
+        newArr.addArr("Ana");
+        newArr.addArr("&");
+        newArr.addArr("Tiago");
+        newArr.addArr("&");
+        newArr.addArr("David");
+        newArr.contains("David");
+        newArr.remove(0);
+        newArr.contains("Hello");
+        newArr.removeByObject("&");
+        newArr.set(0, "Hey");
+        newArr.removeNulls();
+        newArr.addArr("!");
+        newArr.insertObjectByIndex(5, "&");
+        newArr.insertObjectByIndex(7, "&");
+        newArr.removeNulls();
+        newArr.addArr("!");
+        newArr.addArr("!");
+        newArr.set(1, null);
+        newArr.removeNulls();
+        newArr.get(0);
 
-
-         myArrays.addArr("Hello");
-         myArrays.addArr("World");
-         myArrays.addArr("this");
-         myArrays.addArr("is");
-         myArrays.addArr("Ana");
-         myArrays.addArr("&");
-         myArrays.addArr("Tiago");
-         myArrays.addArr("&");
-         myArrays.addArr("David");
-
-         myArrays.contains("David");
 
     }
 }
--- src/MyArrays.java
+++ src/MyArrays.java
@@ -2,55 +2,131 @@ import java.util.*;
 
 public class MyArrays implements List {
 
-    public static Object[] objArr = new Object[3];
+    private Object[] objArr = new Object[1];
+    private int objCounter = 0;
 
-    public static int indexCounter = 0 ;
-
-    public static void addArr(Object obj) {
-        if(objArr.length == indexCounter) {
-            objArr = doubleSpaces(objArr) ;}
-        objArr[indexCounter] = obj;
-        indexCounter++ ;
-        System.out.println(Arrays.toString(MyArrays.objArr));
-    }
-    public static Object[] doubleSpaces(Object[] tempArr){
-        Object[] newArr = new Object[tempArr.length * 2];
-        for (int i = 0; i < tempArr.length ; i++) {
-            newArr[i] = tempArr[i] ;
+    public void addArr(Object obj) {
+        if (objArr.length == objCounter) {
+            objArr = doubleSpaces(objArr);
         }
-        tempArr = newArr ;
+        objArr[objCounter] = obj;
+        objCounter++;
+        System.out.println(Arrays.toString(objArr));
+    }
+
+    private Object[] doubleSpaces(Object[] tempArr) {
+        Object[] newArr = new Object[tempArr.length * 2];
+        for (int i = 0; i < tempArr.length; i++) {
+            newArr[i] = tempArr[i];
+        }
+        tempArr = newArr;
         return tempArr;
     }
 
     @Override
+    public Object[] remove(int index) {
+        Object[] tempArr = new Object[objArr.length];
+        for (int i = 0; i < objArr.length; i++) {
+            if (i == index) {
+                continue;
+            }
+            tempArr[i] = objArr[i];
+        }
+        objCounter--;
+        objArr = tempArr;
+        System.out.println(Arrays.toString(objArr));
+        return objArr;
+    }
+
+    public Object[] removeByObject(Object obj) {
+        Object[] tempArr = new Object[objArr.length];
+        for (int i = 0; i < objArr.length; i++) {
+            if (objArr[i] == obj) {
+                continue;
+            }
+            tempArr[i] = objArr[i];
+        }
+        objCounter--;
+        objArr = tempArr;
+        System.out.println(Arrays.toString(objArr));
+        return objArr;
+    }
+
+    public Object[] removeNulls() {
+        int numberOfNulls = 0;
+        for (Object o : objArr) {
+            if (o == null) {
+                numberOfNulls++;
+            }
+        }
+        pushNullsToEnd();
+        Object[] tempArr = new Object[objArr.length - numberOfNulls];
+        for (int i = 0; i < objArr.length; i++) {
+            if (objArr[i] == null) {
+                continue;
+            }
+            tempArr[i] = objArr[i];
+        }
+        objArr = tempArr;
+        System.out.println(Arrays.toString(objArr));
+        return objArr;
+    }
+
+    private void pushNullsToEnd() {
+        for (int i = 0; i < objArr.length - 1; i++) {
+            if (objArr[i] == null) {
+                for (int j = i; j < objArr.length - 1; j++) {
+                    objArr[j] = objArr[j + 1];
+                }
+            }
+            if (objArr[i] == null && objArr[i + 1] != null) {
+                objArr[i] = objArr[i + 1];
+                objArr[i + 1] = null;
+            }
+        }
+    }
+
+    @Override
+    public boolean contains(Object obj) {
+        for (int i = 0; i < objArr.length; i++) {
+            if (objArr[i] == obj) {
+                System.out.println("\"" + obj.toString() + "\"" + " Exists in your Array");
+                return true;
+            }
+        }
+        System.out.println("\"" + obj.toString() + "\"" + " Doesn't exist in you Array.");
+        return false;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        for (int i = 0; i < objArr.length; i++) {
+            if (i == index) {
+                objArr[i] = element;
+            }
+        }
+        System.out.println(Arrays.toString(objArr));
+        return objArr;
+    }
+
+    public void insertObjectByIndex(int index, Object obj) {
+        for (int i = objArr.length - 1; i > index; i--) {
+            objArr[i] = objArr[i - 1];
+        }
+        objArr[index] = obj;
+        objCounter++;
+        System.out.println(Arrays.toString(objArr));
+    }
+
+    @Override
     public int size() {
-        return 0;
+        return objArr.length;
     }
 
     @Override
     public boolean isEmpty() {
         return false;
     }
-
-    @Override
-    public boolean contains(Object obj) {
-       /*
-       for (Object obj1 : objArr) {
-            if (obj1 == obj) {
-               System.out.println("It exists");
-                return true;
-            }
-        }*/
-
-        for (int i = 0; i < objArr.length ; i++) {
-            if (objArr[i] == obj) {
-                System.out.println("It exists");
-                return true;
-            }
-        }
-        return false;
-    }
-
 
     @Override
     public Iterator iterator() {
@@ -63,12 +139,12 @@ public class MyArrays implements List {
     }
 
     @Override
-    public boolean add(Object o) {
+    public boolean remove(Object o) {
         return false;
     }
 
     @Override
-    public boolean remove(Object o) {
+    public boolean add(Object o) {
         return false;
     }
 
@@ -89,22 +165,13 @@ public class MyArrays implements List {
 
     @Override
     public Object get(int index) {
-        return null;
-    }
-
-    @Override
-    public Object set(int index, Object element) {
-        return null;
+        System.out.println(objArr[index].toString());
+        return objArr[index];
     }
 
     @Override
     public void add(int index, Object element) {
 
-    }
-
-    @Override
-    public Object remove(int index) {
-        return null;
     }
 
     @Override
